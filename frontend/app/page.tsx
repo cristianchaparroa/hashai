d5cd9cebd654d274396f1ad44e4a47e3884beb55
Robert Ramírez <37461123+robertram@users.noreply.github.com>
2024-11-16 15:02:12 -0600

    Transaction front (#23)
    * transactionFront: modal to see the transaction
    
    * transactionFront: redirect after transaction
    
    * transactionFront: buttons moved to bottom
    
    * transactionFront: page title
    
    * transactionFron: merge conflict fix

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -22,12 +22,16 @@ type HomeProps = {
 
 export default function Home({ searchParams }: HomeProps) {
 
-  console.log('searchParams', searchParams.hash?.trim());
-
-  //searchParams?.hash ? searchParams?.hash : 
-
-  const diagramBase64 = searchParams?.hash ? searchParams.hash?.trim() : `Z3JhcGggTFIKICAgIDB4ODYxMS4uLjc3ODQtLT58My4yMCBtRVRIfDB4MjUzNS4uLjMwM2IKICAgIDB4ODYxMS4uLjc3ODQtLT58MjAuMDAgbUVUSHwweDAwMDAuLi5iRTU5CiAgICAweERGZDUuLi45NjNkLS0+fDIuMzggbUVUSHwweDg2MTEuLi43Nzg0CiAgICAweDg2MTEuLi43Nzg0LS0+fDEwLjAwIG1FVEh8MHg3Y0NELi4uNURGQQogICAgMHgyOEM2Li4uMWQ2MC0tPnw0Mi43MyBtRVRIfDB4ODYxMS4uLjc3ODQ=`;
-  const decodedHash = decodeBase64(diagramBase64)
+  const diagramBase64 = searchParams.hash;
+  console.error(diagramBase64);
+  if (!diagramBase64) {
+    return (
+      <div>
+        <p>Incorrect hash</p>
+      </div>
+    )
+  }
+  const decodedHash = decodeMermaidDiagram(diagramBase64)
 
   return (
     <main className={styles.main}>
@@ -38,21 +42,7 @@ export default function Home({ searchParams }: HomeProps) {
   )
 }
 
-
-function decodeBase64(base64String: string): string {
-  if (!base64String) return ''
-  // First, decode the Base64 string to a Uint8Array
-  const binaryString = atob(base64String);
-  const bytes = new Uint8Array(binaryString.length);
-
-  for (let i = 0; i < binaryString.length; i++) {
-    bytes[i] = binaryString.charCodeAt(i);
-  }
-
-  // Convert the Uint8Array to a string using TextDecoder
-  // Go uses UTF-8 encoding by default
-  const decoder = new TextDecoder('utf-8');
-  return decoder.decode(bytes);
+function decodeMermaidDiagram(encodedString: string) {
+  // Use built-in decodeURIComponent to decode the URL-encoded string
+  return decodeURIComponent(encodedString);
 }
-
-
