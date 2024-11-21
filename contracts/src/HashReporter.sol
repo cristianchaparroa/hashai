ccf18dc4aae6fde844e7481a558dd171d584f9ba
cristianchaparroa <cristianchaparroa@gmail.com>
2024-11-21 08:18:19 -0500

    use ownable contract

--- contracts/src/HashReporter.sol
+++ contracts/src/HashReporter.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+
     error NoReportFound();
     error InvalidCategory();
 
-contract HashReporter {
+contract HashReporter is Ownable {
     struct Report {
         uint256 count;
         uint256 category;
@@ -14,7 +16,9 @@ contract HashReporter {
     mapping(address => Report) private reports;
     event ReportCreated(address indexed reportedAddress, uint256 count, uint256 category);
 
-    function createReport(address _reportedAddress, uint256 _category) external {
+    constructor() Ownable(msg.sender) {}
+
+    function createReport(address _reportedAddress, uint256 _category) external onlyOwner {
         Report storage report = reports[_reportedAddress];
         if (!report.exists) {
             reports[_reportedAddress] = Report({
@@ -44,5 +48,15 @@ contract HashReporter {
         else if (_category == 3) return "Fraud";
         else if (_category == 4) return "Other";
         return "";
+    }
+
+    // Function to transfer ownership
+    function transferOwnership(address newOwner) public override onlyOwner {
+        super.transferOwnership(newOwner);
+    }
+
+    // Function to renounce ownership
+    function renounceOwnership() public override onlyOwner {
+        super.renounceOwnership();
     }
 }
