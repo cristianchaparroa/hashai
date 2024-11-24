27b167f283e56e2c0e3bc30dcbbd197ddabd7621
cristianchaparroa <cristianchaparroa@gmail.com>
2024-11-24 07:55:43 -0500

    configuration adjustments

--- backend/config/config.go
+++ backend/config/config.go
@@ -7,14 +7,25 @@ import (
 )
 
 type Config struct {
-	TheGraphApiKey    string `env:"THE_GRAPH_API_KEY"`
-	PolygonRpcURl     string `env:"RPC_URL"`
-	PolygonPrivateKey string `env:"PRIVATE_KEY"`
+	TheGraph struct {
+		ApiKey string `env:"THE_GRAPH_API_KEY"`
+	}
+
+	Polygon struct {
+		RpcURl     string `env:"POLYGON_RPC_URL" yaml:"rpc_url"`
+		ChainID    int64  `env:"POLYGON_CHAIN_ID" yaml:"chain_id"`
+		PrivateKey string `env:"POLYGON_PRIVATE_KEY"`
+	} `yaml:"polygon"`
+
+	ReportContract struct {
+		Address string `env:"REPORT_ADDRESS" yaml:"address"`
+		AbiFile string `env:"REPORT_ABI_FILE" yaml:"abi_file"`
+	} `yaml:"report_contract"`
 }
 
 func New() (*Config, error) {
 	cfg := &Config{}
-	err := cleanenv.ReadEnv(cfg)
+	err := cleanenv.ReadConfig("config/config.yaml", cfg)
 	if err != nil {
 		return nil, fmt.Errorf("error reading config: %w", err)
 	}
